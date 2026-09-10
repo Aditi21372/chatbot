b690d2109072ac41a80fd5bde440cc92a33bece0
Aditi <108211257+Aditi21372@users.noreply.github.com>
2026-09-10 10:37:33 +0530

    Remove leaked secrets and debug artifacts, fix requirements encoding, add .gitignore

--- main.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # This will read variables from your .env file
-my_api_key = os.getenv("OPENAI_API_KEY")
-print("API key is:", my_api_key)
